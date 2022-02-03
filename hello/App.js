a9c7ae23d60f29d9b40582b3b1cb1aa2bd5b6038
Abubakr <58427369+abubakr223@users.noreply.github.com>
2022-02-03 14:31:52 +0200

    Add files via upload

---
+++ hello/App.js
@@ -0,0 +1,109 @@
+import react from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src="holder.js/100px180" />
+    <Card.Body>
+      <Card.Title>Vuma</Card.Title>
+      <Card.Text>
+        Vuma
+      </Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
+  </Card>
+  <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Metro fiber</Card.Title>
+    <Card.Text>
+      Username:TSFibreSupport@mweb.com<br>
+      </br>
+      Password:Mweb@Support123
+    </Card.Text>
+    //https://ticket.metrofibre.co.za:8443/helpdesk/WebObjects/Helpdesk.woa//<br>
+    </br>
+
+    <Button variant="primary">Portal</Button>
+  </Card.Body>
+</Card>
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Octotel</Card.Title>
+    <Card.Text>
+      Email Address: support@octotel.co.za <br></br>
+      Portal: https://periscope.octotel.co.za/login
+    </Card.Text>
+    <Button variant="primary">Portal</Button>
+  </Card.Body>
+</Card>
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Open Serve</Card.Title>
+    <Card.Text>
+      Portal: https://openserve.co.za/uniweb/viewLogin.do?
+    </Card.Text>
+    <Button variant="primary">Portal</Button>
+  </Card.Body>
+</Card>
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Vodacom</Card.Title>
+    <Card.Text>
+      Vodacom
+    </Card.Text>
+    
+  </Card.Body>
+</Card>
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>FrogFoot</Card.Title>
+    <Card.Text>
+    https://isp.frogfoot.com/frogfoot/myaccount/frogfoot/login.isp <br></br>
+    Email: support@frogfoot.com
+    </Card.Text>
+    <Button variant="primary">Portal</Button>
+  </Card.Body>
+</Card>
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>TT connect</Card.Title>
+    <Card.Text>
+        sa
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Link Africa</Card.Title>
+    <Card.Text>
+    Portal: https://portal.linkafrica.co.za/cp/admin/login.php? <br></br>
+    Email: nocdesk@linkafrica.co.za
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Century City & Lightstruct</Card.Title>
+    <Card.Text>
+      Email: support@centurycityconnect.com
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+    </div>
+  );
+}
+
+export default App;
